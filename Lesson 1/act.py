22c8d5e3c2b89b6ce91e32a04dc6ee51ca31be1e
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-06-28 17:36:41 +0300

    add act 1

---
+++ Lesson 1/act.py
@@ -0,0 +1,13 @@
+#Arzaan is happy to start coding today
+print("hello world") 
+
+#just expresing my heart
+print("i love playing football") 
+
+
+print("i like maths") 
+
+
+print("i like pizza") 
+
+
