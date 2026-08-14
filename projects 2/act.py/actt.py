9c130366d375cf95047d8d4cfd0fb27110406cc6
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-08-14 21:52:55 +0530

    added projects 2

---
+++ projects 2/act.py/actt.py
@@ -0,0 +1,14 @@
+#my routine table for the day
+print("my daily timetable")
+
+print("i wake up at 7 am")
+
+print("i go to school at 8 am")
+
+print("i play with my friends in the evening")
+
+print("eat and then got to study for 2 hours")
+
+print ("i play games for 1 hour")
+
+print("i go to sleep at 10 pm")
