f3a76bb6536e1c1d8ffe6f5cc9f208282016ad23
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-08-17 20:24:05 +0530

    added activity 2

---
+++ activity 2/actt.py
@@ -0,0 +1,25 @@
+name = input("name: ")
+club = input("club: ")
+
+number = 5
+points =20.5
+active =3
+events = True
+
+print("name")
+print("club")
+print("number")
+print("points")
+print("events")
+print("active")
+
+number = str(number)
+points = str(points)
+events = str(events)
+active = str(active)
+
+badge = name[:3] + name[-1]
+reverse = club[::-1]
+
+print("Badge:", badge)
+print("Reverse:", reverse)
