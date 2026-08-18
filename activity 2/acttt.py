9ef5647704bc0c1813092cb4a0aabde7b7cacdb2
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-08-18 13:30:50 +0530

    added activity 2

---
+++ activity 2/acttt.py
@@ -0,0 +1,16 @@
+name = input("Name: ")
+goal = input("Goal: ")
+month = input("Month: ")
+
+minutes = 30
+
+print(name)
+print(goal)
+print(month)
+print(minutes)
+
+print("\nGoal Plan")
+print("Not Started")
+print("Practice every day!")
+
+print(name, "will practice", goal, "for", minutes, "minutes.")
---
+++ activity 2/actttt.py
@@ -0,0 +1,35 @@
+team1 = 120
+team2 = 95
+team3 = 140
+team4 = 110
+team5 = 85
+
+total = team1 + team2 + team3 + team4 + team5
+print(total)
+
+average = total / 5
+print(average)
+
+stars = total * 2
+print(stars)
+
+boxes = stars // 25
+leftover = stars % 25
+
+print(boxes)
+print(leftover)
+
+print(total > 500)
+print(total == 500)
+print(total >= 500)
+
+total += 30
+print(total)
+
+total -= 15
+print(total)
+
+stars = total * 2
+boxes = stars // 25
+
+print(boxes)
