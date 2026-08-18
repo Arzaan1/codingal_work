25efa63462f3923db602be9c8f086beed0ee5c50
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-08-18 13:35:09 +0530

    added project

---
+++ projects/act.py
@@ -0,0 +1,11 @@
+name = input("Name: ")
+goal = input("Goal: ")
+month = input("Month: ")
+
+print("Name:", name)
+print("Goal:", goal)
+print("Month:", month)
+print("Practice: 30 minutes")
+
+print("\nGoal Status: Not Started")
+print("Reminder: Practice every day!")
