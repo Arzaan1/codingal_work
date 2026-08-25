f0ad4c9484425193f01199301521f244ade089b2
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-08-25 13:58:39 +0530

    added activity 3

---
+++ activity 3/activity 3/act.py
@@ -0,0 +1,15 @@
+a = 3
+b = 6
+c = 4
+d = 2
+
+answer = (a + b) * c / d
+print(answer)
+
+person = "Sam"
+years = 5
+
+if person == "Sam" or person == "Mike" and years >= 2:
+    print("Hello!")
+else:
+    print("Bye!")
---
+++ activity 3/activity 3/acttt.py
@@ -0,0 +1,13 @@
+average = 25
+wrong = 20
+correct = 30
+count = 10
+
+total = average * count
+print("Total:", total)
+
+total = total - wrong + correct
+print("New total:", total)
+
+average = total / count
+print("New average:", average)
