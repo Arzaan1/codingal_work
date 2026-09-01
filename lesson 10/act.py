9291f929c5dadef7479e166e5a0869d548b628bf
Arzaan1 <nasrullah.faizan25@gmail.com>
2026-09-01 16:02:16 +0300

    added lesson 10

--- lesson 10/act.py
+++ lesson 10/act.py
@@ -1,6 +1,8 @@
 choice =int(input("enter a number whose sum you want to find "))
 
+x =0
+
 for arzaan in range(1,choice+1):
-    x =0
+    
     x =x+arzaan
-    print(f"sum of the {choice} ")
+    print(f"sum of the {choice} is: {x}")
